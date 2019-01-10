06495e6b1b08038de57c5d839b1a3a7261f07216
David Balatero <dbalatero@gmail.com>
2019-01-10 13:10:06 -0800

    initial commit

---
+++ lib/webpack/lite.rb
@@ -0,0 +1,8 @@
+require "webpack/lite/version"
+
+module Webpack
+  module Lite
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/webpack/lite/version.rb
@@ -0,0 +1,5 @@
+module Webpack
+  module Lite
+    VERSION = "0.1.0"
+  end
+end
